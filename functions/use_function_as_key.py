5ccaef1e880faa345dde33dc899678a5f04fc3f7
alex <alex@localhost>
2023-01-17 13:43:43 +0100

    Add example how to use function as a key

---
+++ functions/use_function_as_key.py
@@ -0,0 +1,10 @@
+numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34), (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+
+
+def find_avarages(n):
+    return sum(n) / len(n)
+
+
+print(min(numbers, key=find_avarages))
+print(max(numbers, key=find_avarages))
+
